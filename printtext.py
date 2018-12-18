9f4334d7ae720a2c8da4eb2bb273c2b98c76974f
arason96 <45360147+arason96@users.noreply.github.com>
2018-12-18 15:50:51 +0530

    Add files via upload

---
+++ printtext.py
@@ -0,0 +1,10 @@
+# "Hello, World", "Python is a wonderful language", "I am beginner in Python"
+
+def hello_world():
+
+    x="Hello World! \nPython is a wonderful language. \nI am beginner in Python."
+    return x
+
+a=hello_world()
+
+print (a)
---
+++ python version.py
@@ -0,0 +1,11 @@
+# This is a python program
+
+import platform  # importing package
+
+def pythonversion():
+    return platform.python_version()
+
+
+if __name__ == "__main__":
+    a =pythonversion()
+    print(a)
